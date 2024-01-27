6465ad18622064d8d439f132001e71c9e9e89679
AyoubMDL <ayoubbih119@gmail.com>
2024-01-27 22:00:41 +0100

    change

--- app.py
+++ app.py
@@ -6,8 +6,19 @@ from ocr import ocr_to_df
 def main():
     st.title("OCR Data Extraction App")
 
-    uploaded_files = st.file_uploader("Upload image files", type=["png", "jpg", "jpeg"],
-                                      accept_multiple_files=True)
+    if "file_uploader_key" not in st.session_state:
+        st.session_state["file_uploader_key"] = 0
+
+    if "uploaded_files" not in st.session_state:
+        st.session_state["uploaded_files"] = []
+
+    uploaded_files = st.file_uploader("Upload image files",
+                                      type=["png", "jpg", "jpeg"],
+                                      accept_multiple_files=True,
+                                      key=st.session_state["file_uploader_key"])
+
+    if uploaded_files:
+        st.session_state["uploaded_files"] = uploaded_files
 
     if not uploaded_files:
         st.warning("Please upload valid image files.")
@@ -40,6 +51,8 @@ def download_merged_excel(merged_df):
     excel_filename = "merged_data.xlsx"
     merged_df.to_excel(excel_filename, index=False)
     st.success(f"Download successful: [{excel_filename}](./{excel_filename})")
+    st.session_state["file_uploader_key"] += 1
+    st.rerun()
 
 
 if __name__ == "__main__":
